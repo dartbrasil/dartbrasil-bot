3e91dcc2261cb467b9792f9cf9f33d62dddd66df
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-12-20 14:50:54 -0200

    Init

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dartbrasil_bot/dartbrasil_bot.dart' as dartbrasil_bot;
+
+main(List<String> arguments) {
+  print('Hello world: ${dartbrasil_bot.calculate()}!');
+}
---
+++ test/dartbrasil_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartbrasil_bot/dartbrasil_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
